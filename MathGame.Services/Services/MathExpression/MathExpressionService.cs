b673b6444ccbcad55f91fae248d5be46624f80aa
Bojan Gavreski <bgavreski@gmail.com>
2024-08-06 23:25:15 +0200

    Expression service - answer method refactored

--- MathGame.Services/Services/MathExpression/MathExpressionService.cs
+++ MathGame.Services/Services/MathExpression/MathExpressionService.cs
@@ -6,6 +6,7 @@ using MathGame.Infrastructure.Repositories.Interface;
 using MathGame.Models.Models;
 using MathGame.Models.Models.MathExpression;
 using MathGame.Services.Interface.Services;
+using System.Xml.XPath;
 
 namespace MathGame.Services.Services;
 public class MathExpressionService : IMathExpressionService
@@ -39,77 +40,46 @@ public class MathExpressionService : IMathExpressionService
     {
         await _semaphore.WaitAsync();
 
-        bool shouldHandleCorrectAnswer = false;
-        bool allParticipantsMistookTheExpression = false;
-
         MathExpression mathExpression = await _mathExpressionRepository.Find(answerExpressionRequest.ExpressionUid);
-        AnswerExpressionResponse answerExpressionResponse = 
-                            CreateAnswerExpressionResponse(answerExpressionRequest.ExpressionUid, MathExpressionResultOutcome.Wrong);
+        MathExpressionResponse newMathExpression = null;
+
         Guid gameSessionUid = mathExpression.GameSession.Uid;
 
         try
         {
             if(mathExpression == null)
             {
-                return answerExpressionResponse;
+                throw new KeyNotFoundException($"Expression with Uid: {answerExpressionRequest.ExpressionUid} was not found");
             }
 
             if(IsProvidedAnswerCorrect(answerExpressionRequest.ProvidedAnswer, mathExpression))
             {
-                shouldHandleCorrectAnswer = true;
+                newMathExpression = await GenerateNewMathExpressionAndDeleteCurrent(mathExpression);
+                return await HandleCorrectAnswer(gameSessionUid.ToString(), mathExpression.Uid);
             }
             else
             {
-                IEnumerable<Guid> userInGameSessionUids = await _userInGameSessionRepository.GetUsersInGameSessionUids(gameSessionUid);
-                allParticipantsMistookTheExpression = 
-                    await _mathExpressionRepository.IsMathExpressionMisansweredByEveryUserInGameSession(mathExpression.Uid, userInGameSessionUids);
-                
-                UserMathExpression dbUserMathExpression = new UserMathExpression
+                IEnumerable<Guid> usersInGameSessionUids = await _userInGameSessionRepository.GetUsersInGameSessionUids(gameSessionUid);
+                bool allParticipantsMistookTheExpression = await _mathExpressionRepository.IsMathExpressionMisansweredByEveryUserInGameSession(mathExpression.Uid, usersInGameSessionUids);
+                if (allParticipantsMistookTheExpression)
                 {
-                    Uid = Guid.NewGuid(),
-                    UserInGameSessionFk = await _userInGameSessionService.GetCurrentUserId(),
-                    MathExpressionFk = mathExpression.Id
-                };
+                    newMathExpression = await GenerateNewMathExpressionAndDeleteCurrent(mathExpression);
+                }
 
-                _userMathExpressionRepository.Insert(dbUserMathExpression);
-                return CreateAnswerExpressionResponse(mathExpression.Uid, MathExpressionResultOutcome.Wrong);
+                return await HandleIncorrectAnswer(mathExpression);
             }
         }
         finally{
-            //Notify players here :D 
-            MathExpressionResponse newMathExpression = null;
-            if(shouldHandleCorrectAnswer)
-            {
-                newMathExpression = await GenerateNewMathExpressionAndDeleteCurrent(mathExpression);
-                answerExpressionResponse = CreateAnswerExpressionResponse(mathExpression.Uid, MathExpressionResultOutcome.Correct);
-                await HandleCorrectAnswer(gameSessionUid.ToString(), mathExpression.Uid);
-            }
-
-            if (allParticipantsMistookTheExpression)
-            {
-                newMathExpression = await GenerateNewMathExpressionAndDeleteCurrent(mathExpression);
-            }
-            
             await _mathExpressionRepository.SaveAsync();
 
-            if(shouldHandleCorrectAnswer || allParticipantsMistookTheExpression)
+            if(newMathExpression != null)
             {
                 await _mathGameHubService.SendMathExpressionInGameSessionGroup(gameSessionUid.ToString(), newMathExpression);
             }
 
             _semaphore.Release();
         }
-
-        return answerExpressionResponse;
     }
-
-
-    private async Task<MathExpressionResponse> GenerateNewMathExpressionAndDeleteCurrent(MathExpression mathExpression)
-    {
-        mathExpression.DeletedOn = DateTime.UtcNow;
-        return await GetMathExpression(mathExpression.GameSession);
-    }
-
 
     public async Task<MathExpressionResponse> GetMathExpression(GameSession gameSession)
     {
@@ -125,11 +95,32 @@ public class MathExpressionService : IMathExpressionService
         };
     }
 
-    private async Task HandleCorrectAnswer(string gameSessionUid, Guid mathExpressionUid)
+    private async Task<AnswerExpressionResponse> HandleIncorrectAnswer(MathExpression mathExpression)
+    {
+        UserMathExpression dbUserMathExpression = new UserMathExpression
+        {
+            Uid = Guid.NewGuid(),
+            UserInGameSessionFk = await _userInGameSessionService.GetCurrentUserId(),
+            MathExpressionFk = mathExpression.Id
+        };
+
+        _userMathExpressionRepository.Insert(dbUserMathExpression);
+
+        return CreateAnswerExpressionResponse(mathExpression.Uid, MathExpressionResultOutcome.Wrong);
+    }
+
+    private async Task<MathExpressionResponse> GenerateNewMathExpressionAndDeleteCurrent(MathExpression mathExpression)
+    {
+        mathExpression.DeletedOn = DateTime.UtcNow;
+        return await GetMathExpression(mathExpression.GameSession);
+    }
+
+    private async Task<AnswerExpressionResponse> HandleCorrectAnswer(string gameSessionUid, Guid mathExpressionUid)
     {
         // TODO: This should be changed to exclude all of the players that have answered
         await _mathGameHubService.NotifyAllPlayersMissed(gameSessionUid.ToString(), mathExpressionUid);
         await _userInGameSessionService.IncreaseCurrentUserScore();
+        return CreateAnswerExpressionResponse(mathExpressionUid, MathExpressionResultOutcome.Correct);
     }
 
     private AnswerExpressionResponse CreateAnswerExpressionResponse(Guid expressionUid, MathExpressionResultOutcome outcome)
@@ -141,11 +132,10 @@ public class MathExpressionService : IMathExpressionService
         };
     }
 
-    private bool IsProvidedAnswerCorrect(bool providedAnswer, MathExpression mathExpression)
+    private static bool IsProvidedAnswerCorrect(bool providedAnswer, MathExpression mathExpression)
     {
-        bool correctExpressionResult = mathExpression.ActualResult == mathExpression.DisplayedResult;
+        bool isExpressionDisplayResultCorrect = mathExpression.ActualResult == mathExpression.DisplayedResult;
 
-        // XAND 
-        return !(providedAnswer ^ correctExpressionResult);
+        return isExpressionDisplayResultCorrect == providedAnswer;
     }
 }
